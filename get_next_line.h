4b56cae3184c6406325361cba7b7a3ab616cc738
serial3 <diogo_R_Leal@hotmail.com>
2021-11-14 08:47:24 -0800

    utils up

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: dromao-l <dromao-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 19:27:29 by dromao-l          #+#    #+#             */
-/*   Updated: 2021/11/03 19:31:35 by dromao-l         ###   ########.fr       */
+/*   Updated: 2021/11/14 08:15:45 by dromao-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,10 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
+
+void	ft_putstr(char *str);
+char	*str_appendc(char *str, char c);
+int		ft_strlen(char *str);
+char	*ft_strchr(const char *s, int c);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,65 @@
 /*   By: dromao-l <dromao-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 19:32:20 by dromao-l          #+#    #+#             */
-/*   Updated: 2021/11/03 19:32:56 by dromao-l         ###   ########.fr       */
+/*   Updated: 2021/11/14 08:14:23 by dromao-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+		write(1, str++, 1);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str == 0)
+		return (0);
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			return ((char *)s + i);
+		i++;
+	}
+	if (c == '\0')
+		return ((char *)s + i);
+	return (NULL);
+}
+
+char	*str_appendc(char *str, char c)
+{
+	char	*temp;
+	int		i;
+
+	temp = (char *)malloc(sizeof(char) * (ft_strlen(str) + 2));
+	if (temp == NULL)
+		return (NULL);
+	i = 0;
+	if (str != 0)
+	{
+		while (str[i] != '\0')
+		{
+			temp[i] = str[i];
+			i++;
+		}
+		free(str);
+	}
+	temp[i++] = c;
+	temp[i] = '\0';
+	return (temp);
+}
